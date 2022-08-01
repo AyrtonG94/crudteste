aab171fad06fd5270c6ef932f6ea678918baa022
AyrtonG94 <ayrton.gomes@live.com>
2022-08-01 18:49:45 -0300

    Ajustes no metodo de relacionamento dos models Pessoa e Endereco

--- CrudTeste/database/migrations/2022_08_01_203950_pessoas_enderecos.php
+++ CrudTeste/database/migrations/2022_08_01_203950_pessoas_enderecos.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('pessoa_enderecos', function(Blueprint $table) {
+        Schema::create('pessoas_enderecos', function(Blueprint $table) {
             $table->id();
 
             $table->foreignId('pessoa_id')->constrained('pessoas');
