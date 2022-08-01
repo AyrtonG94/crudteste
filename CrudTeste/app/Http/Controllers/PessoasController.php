89471aaacbc95866d020e0020e23eb68c1ee6ca3
AyrtonG94 <ayrton.gomes@live.com>
2022-08-01 20:34:12 -0300

    Criacao do controller Pessoas, criacao das rotas index e cadastro

--- CrudTeste/app/Http/Controllers/PessoasController.php
+++ CrudTeste/app/Http/Controllers/PessoasController.php
@@ -4,15 +4,36 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Pessoa;
+use Exception;
+
+use function PHPUnit\Framework\isEmpty;
+
 
 class PessoasController extends Controller
 {
-    public function index() {
+    public function index()
+    {
+        try {
 
-        Pessoa::all();
+            $pessoas = Pessoa::all();
+
+            if (!$pessoas) {
+                return "Não existe pessoas cadastradas no momento";
+            } else {
+                return $pessoas;
+            }
+        } catch (Exception) {
+        }
     }
 
-    public function cadastrar(Request $request) {
-        Pessoa::create($request->all());
+    public function cadastrar(Request $request)
+    {
+
+        try {
+            $pessoa =  Pessoa::create($request->all());
+            return $pessoa;
+        } catch (Exception) {
+            return "Não  foi possível criar o cadastro, preencha todos os campos corretamente";
+        }
     }
 }
